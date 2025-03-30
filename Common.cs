ea31f03f67b62c950a8a80a2ba23773f2ca94015
Scott McCutchen <scott.mccutchen@soverance.com>
2025-03-30 13:49:12 -0400

    remove unnecessary includes

--- Common.cs
+++ Common.cs
@@ -6,19 +6,11 @@
 namespace Soverance.Certifactory;
 
 using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Net;
-using System.Net.NetworkInformation;
-using System.Net.Sockets;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web;
 
 public class Common
 {
