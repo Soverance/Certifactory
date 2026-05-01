d3bb2839df8380cc3b3ad01e16c11cdf9a630450
Scott McCutchen <scott.mccutchen@soverance.com>
2026-04-30 23:37:01 -0400

    more tests

--- Certifactory.Tests/CertificateBuilderTests.cs
+++ Certifactory.Tests/CertificateBuilderTests.cs
@@ -3,6 +3,7 @@
 // Enterprise Applications Architect - Soverance Studios
 // scott.mccutchen@soverance.com
 
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using FluentAssertions;
 using Soverance.Certifactory.Pq;
@@ -70,5 +71,45 @@ public class CertificateBuilderTests
         leafAki.KeyIdentifier!.Value.ToArray().Should().Equal(caSki.ToArray());
         leaf.Extensions.OfType<X509BasicConstraintsExtension>().Single()
             .CertificateAuthority.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Smime_cert_includes_email_in_san_and_correct_eku()
+    {
+        var caSigner = SignerFactory.Create(KnownAlgorithms.Rsa4096);
+        caSigner.GenerateKeyPair();
+        var caCert = CertificateBuilder.BuildCertificate(new CertificateSpec(
+            CertificatePurpose.RootCa, "smime-test-ca", "Pass", caSigner,
+            ServerIp: null, EmailAddress: null, Issuer: null));
+
+        var leafSigner = SignerFactory.Create(KnownAlgorithms.Rsa4096);
+        leafSigner.GenerateKeyPair();
+        var leaf = CertificateBuilder.BuildCertificate(new CertificateSpec(
+            CertificatePurpose.Smime, "alice@example.com", "Pass", leafSigner,
+            ServerIp: null,
+            EmailAddress: "alice@example.com",
+            Issuer: new IssuerInfo(caCert, caSigner)));
+
+        leaf.Subject.Should().Contain("CN=alice@example.com");
+
+        var ekus = leaf.Extensions
+            .OfType<X509EnhancedKeyUsageExtension>()
+            .Single().EnhancedKeyUsages;
+        ekus.OfType<Oid>()
+            .Should().Contain(o => o.Value == "1.3.6.1.5.5.7.3.4"); // emailProtection
+
+        var ku = leaf.Extensions.OfType<X509KeyUsageExtension>().Single().KeyUsages;
+        ku.Should().HaveFlag(X509KeyUsageFlags.DigitalSignature);
+        ku.Should().HaveFlag(X509KeyUsageFlags.NonRepudiation);
+        ku.Should().HaveFlag(X509KeyUsageFlags.KeyEncipherment);
+        ku.Should().HaveFlag(X509KeyUsageFlags.DataEncipherment);
+
+        var sanExt = leaf.Extensions.Single(e => e.Oid != null && e.Oid.Value == "2.5.29.17");
+        var bcSan = Org.BouncyCastle.Asn1.X509.GeneralNames.GetInstance(
+            Org.BouncyCastle.Asn1.Asn1Object.FromByteArray(sanExt.RawData));
+        var rfc822Names = bcSan.GetNames()
+            .Where(n => n.TagNo == Org.BouncyCastle.Asn1.X509.GeneralName.Rfc822Name)
+            .Select(n => n.Name.ToString());
+        rfc822Names.Should().Contain("alice@example.com");
     }
 }
